f177cf73d01e3fe7e021de5a048c1cb2f971862c
JarmanjitSingh <jarmanjits176@gmail.com>
2023-08-17 23:36:27 +0530

    Ordr model and three api routes created

---
+++ src/app/api/order/create-order/route.js
@@ -0,0 +1,48 @@
+import connectToDB from "@/database";
+import AuthUser from "@/middleware/AuthUser";
+import Cart from "@/models/cart";
+import Order from "@/models/order";
+import { NextResponse } from "next/server";
+
+
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req){
+    try {
+        await connectToDB();
+
+        const isAuthUser = await AuthUser(req);
+
+        if(isAuthUser){
+            const data = await req.json();
+            const {user} = data;
+            const saveNewOrder = await Order.create(data);
+
+            if(saveNewOrder){
+                await Cart.deleteMany({userId: user})
+
+                return NextResponse.json({
+                    success: true,
+                    message: 'Products are on the way!'
+                })
+            }else{
+                return NextResponse.json({
+                    success: false,
+                    message: 'Failed to Create order! try again later.'
+                })
+            }
+        }else{
+            return NextResponse.json({
+                success: false,
+                message: "You are not authenticated."
+            })
+        }
+        
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: "Something went wrong! please try again later."
+        })
+    }
+}
---
+++ src/app/api/order/order-details/route.js
@@ -0,0 +1,52 @@
+import connectToDB from "@/database";
+import AuthUser from "@/middleware/AuthUser";
+import Order from "@/models/order";
+
+
+
+export const dynamic = "force-dynamic";
+
+
+export async function GET(req){
+    try {
+        await connectToDB();
+
+        const isAuthUser = await AuthUser(req);
+
+        if(isAuthUser){
+            const {searchParams} = new URL(req.url);
+            const id = searchParams.get('id')
+
+            if(!id) return NextResponse.json({
+                success: false,
+                message: "Product is required."
+            })
+
+            const extractOrderDetails = await Order.findById(id).populate('orderItems.product')
+
+            if(extractOrderDetails){
+                return NextResponse.json({
+                    success: true,
+                    data: extractOrderDetails
+                })
+            }else{
+                return NextResponse.json({
+                    success: false,
+                    message: "Failed to get order Details! try again later."
+                })
+            }
+
+        }else{
+            return NextResponse.json({
+                success: false,
+                message: "You are not Authenticated."
+            })
+        }
+        
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: "Something went wrong! please try again later."
+        })
+    }
+}
